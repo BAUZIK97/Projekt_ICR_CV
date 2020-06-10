517575f64ef4c6a0332630954cd9389f41a9eab7
bauzik97 <marcin.bauza.97@gmail.com>
2020-06-10 23:08:30 +0200

    Update CEye.py

--- CEye.py
+++ CEye.py
@@ -1,3 +1,12 @@
 import cv2
 
 
+class CEye:
+    def __init__(self, img, direction, x):
+        self.ccolor = cv2.resize(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), (50, 42), interpolation=cv2.INTER_AREA)
+        self.cclass = direction
+        self.cwhich = []
+        self.x = x
+
+    def add_lr(self, lr):
+        self.cwhich = lr
